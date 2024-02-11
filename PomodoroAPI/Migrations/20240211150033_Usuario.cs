4e78a8966272119836f61236f2a95083a9b50eec
Carlos Roberto <crpu4a@gmail.com>
2024-02-11 12:24:46 -0300

    adicionar migration atualizando usuário

---
+++ PomodoroAPI/Migrations/20240211150033_Usuario.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PomodoroAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Usuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "usuarios",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuarios_Email",
+                table: "usuarios",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_usuarios_Email",
+                table: "usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "usuarios",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64);
+        }
+    }
+}
