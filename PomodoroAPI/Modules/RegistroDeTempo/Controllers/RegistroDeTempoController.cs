6d7ec256d67660a2c61589a401ab6df638e0478e
Carlos Roberto <crpu4a@gmail.com>
2024-02-15 08:33:19 -0300

    feat: criação de controller de registro de tempo WIP

---
+++ PomodoroAPI/Modules/RegistroDeTempo/Controllers/RegistroDeTempoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PomodoroAPI.Modules.RegistroDeTempo.Models;
+using PomodoroAPI.Modules.RegistroDeTempo.Repositories;
+
+namespace PomodoroAPI.Modules.RegistroDeTempo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RegistroDeTempoController : ControllerBase
+{
+    private readonly IRegistroDeTempoRepository _registroDeTempoRepository;
+
+    public RegistroDeTempoController(IRegistroDeTempoRepository registroDeTempoRepository)
+    {
+        _registroDeTempoRepository = registroDeTempoRepository;
+    }
+
+    [HttpGet]
+    public ActionResult<List<RegistroDeTempoModel>> Index(int page, int perPage = 12)
+    {
+        return Ok(_registroDeTempoRepository.Listar(page, perPage));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<RegistroDeTempoModel>> Adicionar([FromBody] RegistroDeTempoModelView registro)
+    {
+        return Ok(await _registroDeTempoRepository.Adicionar(registro));
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<ActionResult<RegistroDeTempoModel>> Atualizar(int id, [FromBody] RegistroDeTempoModelView registro)
+    {
+        return Ok(await _registroDeTempoRepository.Atualizar(id, registro));
+    }
+    
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<ActionResult<RegistroDeTempoModel>> Apagar(int id)
+    {
+        return Ok(await _registroDeTempoRepository.Apagar(id));
+    }
+}
