b785435bad0920d0e7ce44ca3ae8ed63f7c22d94
Carlos Roberto <crpu4a@gmail.com>
2024-02-16 00:34:45 -0300

    feat: criar controller de periodos de tempo com atualizar e apagar.

---
+++ PomodoroAPI/Modules/RegistroDeTempo/Controllers/PeriodoDeTempoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PomodoroAPI.Modules.RegistroDeTempo.Models;
+using PomodoroAPI.Modules.RegistroDeTempo.Repositories;
+
+namespace PomodoroAPI.Modules.RegistroDeTempo.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class PeriodoDeTempoController : ControllerBase
+{
+    private readonly IPeriodoDeTempoRepository _periodoDeTempoRepository;
+
+    public PeriodoDeTempoController(IPeriodoDeTempoRepository periodoDeTempoRepository)
+    {
+        _periodoDeTempoRepository = periodoDeTempoRepository;
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<ActionResult<PeriodoDeTempoModel>> Atualizar(int id, [FromBody] PeriodoDeTempoModelView periodo)
+    {
+        return Ok(await _periodoDeTempoRepository.Atualizar(id,
+            new PeriodoDeTempoModel() { Inicio = periodo.Inicio, Fim = periodo.Fim }
+        ));
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<ActionResult<PeriodoDeTempoModel>> Apagar(int id)
+    {
+        return Ok(await _periodoDeTempoRepository.Apagar(id));
+    }
+}
