074efd1a039c00b1a254612b9fb6480246639a70
Carlos Roberto <crpu4a@gmail.com>
2024-02-22 07:38:00 -0300

    feat: criação de custom controller

---
+++ PomodoroAPI/Modules/Shared/Controllers/CustomController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PomodoroAPI.Infrastructure.Http;
+
+namespace PomodoroAPI.Modules.Shared.Controllers;
+
+public class CustomController : ControllerBase
+{
+    protected ActionResult<T> HandleResponse<T>(Result<T> result)
+    {
+        if (result.IsValid)
+            return Ok(result.Data);
+
+        var errorResponse = new HttpErrorResponse() { Message = result.Message };
+        if (result.Message!.Contains("not_found")) return NotFound(errorResponse);
+        if (result.Message!.Contains("unauthorized")) return Unauthorized();
+        return BadRequest(errorResponse);
+    }
+}
