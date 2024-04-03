8251a53e9535ee21b04c043fbfa3d7f75e29ddd2
Carlos Júnior <crpu4a@gmail.com>
2024-04-03 20:32:42 -0300

    fix: migrations

---
+++ API/Migrations/20240403231659_Initial.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false),
+                    Email = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Senha = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    Nome = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categorias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_categorias_usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "registros_de_tempo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    CategoriaId = table.Column<int>(type: "integer", nullable: true),
+                    Descricao = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_registros_de_tempo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_registros_de_tempo_categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "categorias",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_registros_de_tempo_usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "periodos_de_tempo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    RegistroDeTempoId = table.Column<int>(type: "integer", nullable: false),
+                    Inicio = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Fim = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_periodos_de_tempo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_periodos_de_tempo_registros_de_tempo_RegistroDeTempoId",
+                        column: x => x.RegistroDeTempoId,
+                        principalTable: "registros_de_tempo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_periodos_de_tempo_usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categorias_UsuarioId",
+                table: "categorias",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_periodos_de_tempo_RegistroDeTempoId",
+                table: "periodos_de_tempo",
+                column: "RegistroDeTempoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_periodos_de_tempo_UsuarioId",
+                table: "periodos_de_tempo",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_registros_de_tempo_CategoriaId",
+                table: "registros_de_tempo",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_registros_de_tempo_UsuarioId",
+                table: "registros_de_tempo",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuarios_Email",
+                table: "usuarios",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "periodos_de_tempo");
+
+            migrationBuilder.DropTable(
+                name: "registros_de_tempo");
+
+            migrationBuilder.DropTable(
+                name: "categorias");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
