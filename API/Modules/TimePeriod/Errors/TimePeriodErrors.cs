7817fbd1789cae750f646f024d7810aeb25d98b5
Carlos Júnior <crpu4a@gmail.com>
2024-05-16 20:56:24 -0300

    feat: update time periods errors

--- API/Modules/TimePeriod/Errors/TimePeriodErrors.cs
+++ API/Modules/TimePeriod/Errors/TimePeriodErrors.cs
@@ -2,12 +2,7 @@
 
 public static class TimePeriodErrors
 {
-    public static readonly string EndDateIsBiggerThenStartDate =
-        "bad_request: A data final do timePeriod deve ser maior que a inicial.";
-
-    public static readonly string NotFound =
-        "not_found: Não foi possível encontrar um timePeriod com esse id.";
-    
-    public static readonly string WrongTimeRecordId = 
-        "É necessário informar um timeRecordId válido para cadastrar um período.";
+    public const string EndDateIsBiggerThenStartDate = "bad_request: A data final do timePeriod deve ser maior que a inicial.";
+    public const string NotFound = "not_found: Não foi possível encontrar um timePeriod com esse id.";
+    public const string WrongTimeRecordId = "É necessário informar um timeRecordId válido para cadastrar um período.";
 }
