2e1e9a4ef2d44528b45be6f88d347067f0be0daa
Carlos Júnior <crpu4a@gmail.com>
2024-05-16 21:29:01 -0300

    feat: add user role

--- API/Modules/User/DTO/UserDto.cs
+++ API/Modules/User/DTO/UserDto.cs
@@ -1,8 +1,12 @@
-﻿namespace API.Modules.User.DTO;
+﻿using API.Modules.User.Entities;
+
+namespace API.Modules.User.DTO;
 
 public class UserDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public UserRole UserRole { get; set; }
+    public string UserRoleLabel => UserRole.ToString();
 }
--- API/Modules/User/Entities/UserEntity.cs
+++ API/Modules/User/Entities/UserEntity.cs
@@ -1,20 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Modules.User.Entities;
 
+public enum UserRole
+{
+    Admin,
+    NormalUser,
+    PayingUser,
+    GuestUser
+}
+
 [Table("users"), Index(nameof(Email), IsUnique = true)]
 public class UserEntity
 {
-    [Key] public int Id { get; set; }
+    [Key] 
+    public int Id { get; set; }
 
     [Required, MinLength(3), MaxLength(120)]
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 
     [Required, EmailAddress, MinLength(8), MaxLength(64)]
-    public string Email { get; set; }
+    public string Email { get; set; } = null!;
 
     [Required, MinLength(8), MaxLength(32)]
-    public string? Password { get; set; }
+    public string Password { get; set; } = null!;
+    
+    [Required]
+    public UserRole UserRole { get; set; } = UserRole.NormalUser;
 }
