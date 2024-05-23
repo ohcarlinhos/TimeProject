41cc2fd9ac4256696b8c949a773dec4863140e7f
Carlos Júnior <crpu4a@gmail.com>
2024-05-23 12:35:06 -0300

    feat: model validations

--- API/Modules/User/Models/CreateUserModel.cs
+++ API/Modules/User/Models/CreateUserModel.cs
@@ -4,8 +4,10 @@ namespace API.Modules.User.Models;
 
 public class CreateUserModel
 {
+    [MinLength(2), MaxLength(120)]
     public string Name { get; set; }
-    [EmailAddress]
+    [EmailAddress, MaxLength(64)]
     public string Email { get; set; }
+    [MinLength(8), MaxLength(48)]
     public string Password { get; set; }
 }
--- API/Modules/User/Models/UpdateUserModel.cs
+++ API/Modules/User/Models/UpdateUserModel.cs
@@ -4,9 +4,12 @@ namespace API.Modules.User.Models;
 
 public class UpdateUserModel
 {
+    [MinLength(2), MaxLength(120)]
     public string? Name { get; set; }
-    [EmailAddress]
+    [EmailAddress, MaxLength(64)]
     public string? Email { get; set; }
+    [MinLength(8), MaxLength(48)]
     public string? Password { get; set; }
+    [MinLength(8), MaxLength(48)]
     public string? OldPassword { get; set; }
 }
