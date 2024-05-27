cc645a48bd9e721fa48f38114c2b8af007cccb44
Carlos Júnior <crpu4a@gmail.com>
2024-05-26 22:54:34 -0300

    feat: add blazorise

--- AdminPanel/Program.cs
+++ AdminPanel/Program.cs
@@ -1,10 +1,18 @@
 using AdminPanel.Components;
+using Blazorise;
+using Blazorise.Tailwind;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
 
 var app = builder.Build();
 
