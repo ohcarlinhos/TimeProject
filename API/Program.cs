d529c22373caef09e2b8ba9f3cd80a202d9f8f07
Carlos Júnior <crpu4a@gmail.com>
2024-06-07 19:15:23 -0300

    refactor: clear comments

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,18 @@
 using System.Text.Json.Serialization;
-using API.Database;
 using Microsoft.EntityFrameworkCore;
+
+using API.Database;
 using API.Infrastructure;
 using API.Infrastructure.Config;
 using API.Infrastructure.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// configuração das variáveis de ambiente
 EnvConfig.Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
 
 const string customCors = "_customCors";
 CorsBuilderConfig.Apply(builder, customCors);
 JwtBuilderConfig.Apply(builder);
 
-// Adição de suporte a retornos de arrays nos JSONs
 builder.Services.AddControllers()
     .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
@@ -22,7 +20,6 @@ builder.Services.AddEndpointsApiExplorer();
 
 SwaggerBuilderConfig.Apply(builder);
 
-// Configuração par utilização do Postgres
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ProjectContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("DataBase"))
 );
@@ -32,18 +29,18 @@ ServicesBuilderConfig.Apply(builder);
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-// build da aplicação
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // erro em dev
     app.UseExceptionHandler("/error-development");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else app.UseExceptionHandler("/error"); // erro em produção
+else
+{
+    app.UseExceptionHandler("/error");
+}
 
 app.UseCors(customCors);
 app.UseHttpsRedirection();
