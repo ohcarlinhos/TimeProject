f0dc79e8f448255de671a5a32aa33cd8d2e12537
Carlos Roberto <crpu4a@gmail.com>
2024-07-25 00:47:48 -0300

    feat: add external link migration

--- API/Database/ProjectContext.cs
+++ API/Database/ProjectContext.cs
@@ -17,16 +17,15 @@ public class ProjectContext(DbContextOptions<ProjectContext> options) : DbContex
         mb.ApplyConfiguration(new UserConfiguration());
         mb.ApplyConfiguration(new TimeRecordConfiguration());
         mb.ApplyConfiguration(new TimePeriodConfiguration());
-
         mb.ApplyConfiguration(new CategoryConfiguration());
         mb.ApplyConfiguration(new RegisterCodeConfiguration());
 
         mb.Entity<RegisterCode>().HasData([
-            new RegisterCode(),
-            new RegisterCode(),
-            new RegisterCode(),
-            new RegisterCode(),
-            new RegisterCode(),
+            new RegisterCode { Id = "07577660-b921-4e07-bb68-990e8f286475" },
+            new RegisterCode { Id = "209a8b3f-9a5c-4019-b673-846c1b3d92f0" },
+            new RegisterCode { Id = "2b9134e6-4a21-417a-a08c-8600d05247fe" },
+            new RegisterCode { Id = "6daea389-c618-4f44-8958-d423952a4941" },
+            new RegisterCode { Id = "d62a3d33-18c9-4ab3-85ec-c8f22187f078" },
         ]);
     }
 }
