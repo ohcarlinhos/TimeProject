d05991f76a605f47af3efeccf96d62596abfbd94
Carlos Júnior <crpu4a@gmail.com>
2024-08-03 20:41:13 -0300

    feat: add RegisterCodeController.cs

---
+++ API/Modules/User/Controllers/RegisterCodeController.cs
@@ -0,0 +1,23 @@
+﻿using API.Database;
+using API.Modules.Shared;
+using API.Modules.Shared.Controllers;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Modules.User.Controllers;
+
+[ApiController]
+[Route("api/register-code")]
+public class RegisterCodeController(ProjectContext dbContext) : CustomController
+{
+    [HttpGet, Authorize]
+    public ActionResult<List<RegisterCode>> Index()
+    {
+        if (UserRole.Admin.ToString() == UserSession.Role(User))
+            return Ok(dbContext.RegisterCodes.Include((e) => e.User).ToList());
+
+        return Unauthorized();
+    }
+}
