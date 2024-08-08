27a49cbc88124cc1d88bb0b6379e10e40f6387d9
Carlos Roberto <crpu4a@gmail.com>
2024-08-08 19:15:14 -0300

    fix: message error http response

--- API/Modules/Shared/Controllers/CustomController.cs
+++ API/Modules/Shared/Controllers/CustomController.cs
@@ -8,19 +8,16 @@ public class CustomController : ControllerBase
 {
     protected ActionResult<T> HandleResponse<T>(Result<T> result)
     {
-
-        string?[] message  = result.Message.Split(":");
-        
         if (string.IsNullOrEmpty(result.ActionName) && result.IsValid)
-        {
             return CreatedAtAction(result.ActionName, result.Data);
-        }
 
         if (result.IsValid) return Ok(result.Data);
 
-        var errorResponse = new HttpErrorResponse() { Message = message[1] ?? message[0] };
-        if (message[0]!.Contains("not_found")) return NotFound(errorResponse);
-        if (message[0]!.Contains("unauthorized")) return Unauthorized();
+        var messageSplit = result.Message.Split(":");
+        var errorResponse = new HttpErrorResponse() { Message = messageSplit[messageSplit.Length - 1] };
+        
+        if (result.Message.Contains("not_found")) return NotFound(errorResponse);
+        if (result.Message.Contains("unauthorized")) return Unauthorized();
         return BadRequest(errorResponse);
     }
 }
