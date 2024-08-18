698d73b82c3082410708da573a6ea8b6ff44d5bc
Carlos Júnior <crpu4a@gmail.com>
2024-08-18 16:11:25 -0300

    feat: add page set to PaginationQuery

--- Shared/General/PaginationQuery.cs
+++ Shared/General/PaginationQuery.cs
@@ -2,7 +2,14 @@
 
 public class PaginationQuery
 {
-    public int Page { get; set; } = 1;
+    private int _page = 1;
+
+    public int Page
+    {
+        get => _page;
+        set => _page = value <= 0 ? 1 : value;
+    }
+
     public int PerPage { get; set; } = 10;
     public string? Search { get; set; } = "";
     public string? OrderBy { get; set; } = "";
