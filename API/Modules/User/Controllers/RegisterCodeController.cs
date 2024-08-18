b82897aa42a4b2915823213d466dc2b5bf57e35d
Carlos Júnior <crpu4a@gmail.com>
2024-08-18 13:44:48 -0300

    feat: add index pagination and delete to register code controller

--- API/Modules/User/Controllers/RegisterCodeController.cs
+++ API/Modules/User/Controllers/RegisterCodeController.cs
@@ -4,6 +4,7 @@ using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using Shared.General;
 using Shared.General.Util;
 using Shared.User;
 
@@ -14,12 +15,20 @@ namespace API.Modules.User.Controllers;
 public class RegisterCodeController(ProjectContext dbContext) : CustomController
 {
     [HttpGet, Authorize]
-    public ActionResult<List<RegisterCode>> Index()
+    public ActionResult<List<RegisterCode>> Index([FromQuery] PaginationQuery paginationQuery)
     {
-        if (UserRole.Admin.ToString() == UserClaims.Role(User))
-            return Ok(dbContext.RegisterCodes.Include((e) => e.User).ToList());
+        if (UserRole.Admin.ToString() != UserClaims.Role(User))
+            return Forbid();
 
-        return Forbid();
+        var query = dbContext.RegisterCodes.AsQueryable();
+
+        var data = query
+            .Skip((paginationQuery.Page - 1) * paginationQuery.PerPage)
+            .Take(paginationQuery.PerPage)
+            .Include((e) => e.User)
+            .ToList();
+
+        return Ok(Pagination<RegisterCode>.Handle(data, paginationQuery, query.Count()));
     }
 
     [HttpPost, Authorize]
@@ -32,5 +41,19 @@ public class RegisterCodeController(ProjectContext dbContext) : CustomController
         dbContext.RegisterCodes.Add(registerCode);
         await dbContext.SaveChangesAsync();
         return Ok(registerCode);
+    }
+
+    [HttpDelete, Authorize, Route("{id}")]
+    public async Task<ActionResult<bool>> Delete([FromRoute] string id)
+    {
+        if (UserRole.Admin.ToString() != UserClaims.Role(User))
+            return Forbid();
+
+        var registerCode = new RegisterCode { Id = id };
+
+        dbContext.RegisterCodes.Remove(registerCode);
+        await dbContext.SaveChangesAsync();
+
+        return NoContent();
     }
 }
