ed453c6fa0c1a5c53d2d4b24fa144e1e510867ac
Carlos Júnior <crpu4a@gmail.com>
2024-08-18 13:44:10 -0300

    feat: add default values to PaginationQuery.cs

--- Shared/General/PaginationQuery.cs
+++ Shared/General/PaginationQuery.cs
@@ -2,9 +2,9 @@
 
 public class PaginationQuery
 {
-    public int Page { get; set; }
-    public int PerPage { get; set; }
-    public string? Search { get; set; }
-    public string? OrderBy { get; set; }
+    public int Page { get; set; } = 1;
+    public int PerPage { get; set; } = 10;
+    public string? Search { get; set; } = "";
+    public string? OrderBy { get; set; } = "";
     public string? Sort { get; set; } = "desc";
 }
