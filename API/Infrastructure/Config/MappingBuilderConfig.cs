87158cc6e1a7c2140180f3557d4e3e41ea8c9f53
Carlos Júnior <crpu4a@gmail.com>
2024-09-15 22:34:34 -0300

    feat: create mapper builder config

---
+++ API/Infrastructure/Config/MappingBuilderConfig.cs
@@ -0,0 +1,11 @@
+﻿using API.Infrastructure.Mapping;
+
+namespace API.Infrastructure.Config;
+
+public static class MappingBuilderConfig
+{
+    public static void Apply(WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(typeof(MappingProfile));
+    }
+}
