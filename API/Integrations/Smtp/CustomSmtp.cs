01ad91806141e84cb393f89c710159b879c2befe
Carlos Júnior <crpu4a@gmail.com>
2024-09-15 22:32:53 -0300

    feat: create smtp integration

---
+++ API/Integrations/Smtp/CustomSmtp.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace API.Integrations.Smtp;
+
+public class CustomSmtp : ICustomSmtp
+{
+    private readonly SmtpClient _smtpClient;
+    private readonly IConfiguration _configuration;
+
+    public CustomSmtp(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        _smtpClient = new SmtpClient();
+        _smtpClient.Host = configuration["Smtp:Host"]!;
+        _smtpClient.Port = int.Parse(configuration["Smtp:Port"]!);
+        _smtpClient.EnableSsl = true;
+        _smtpClient.Credentials =
+            new NetworkCredential(configuration["Smtp:Username"]!, configuration["Smtp:Password"]!);
+    }
+
+    public void Send(MailMessage mailMessage)
+    {
+        mailMessage.From = new MailAddress(_configuration["Smtp:Username"]!);
+        _smtpClient.Send(mailMessage);
+    }
+}
---
+++ API/Integrations/Smtp/ICustomSmtp.cs
@@ -0,0 +1,8 @@
+﻿using System.Net.Mail;
+
+namespace API.Integrations.Smtp;
+
+public interface ICustomSmtp
+{ 
+    void Send(MailMessage mailMessage);
+}
