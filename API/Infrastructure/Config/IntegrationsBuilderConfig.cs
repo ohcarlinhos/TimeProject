51a4b242f2ca0f004d0be1388fd48eb63ab22d2d
Carlos Júnior <crpu4a@gmail.com>
2024-09-15 22:33:44 -0300

    feat: create integration builder config

---
+++ API/Infrastructure/Config/IntegrationsBuilderConfig.cs
@@ -0,0 +1,11 @@
+﻿using API.Integrations.Smtp;
+
+namespace API.Infrastructure.Config;
+
+public static class IntegrationsBuilderConfig
+{
+    public static void Apply(WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ICustomSmtp, CustomSmtp>();
+    }
+}
