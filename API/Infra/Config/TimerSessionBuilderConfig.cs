a9acdf127eeaef51211f876b6255fe652d9f5753
Carlos Júnior <crpu4a@gmail.com>
2024-10-16 16:00:32 -0300

    refactor: clear imports

--- API/Infra/Config/TimerSessionBuilderConfig.cs
+++ API/Infra/Config/TimerSessionBuilderConfig.cs
@@ -1,9 +1,4 @@
-﻿using API.Core.TimePeriod;
-using API.Core.TimePeriod.Utils;
-using API.Core.TimerSession;
-using API.Modules.TimePeriod;
-using API.Modules.TimePeriod.Utils;
-
+﻿using API.Core.TimerSession;
 using API.Modules.TimerSession;
 
 namespace API.Infra.Config;
