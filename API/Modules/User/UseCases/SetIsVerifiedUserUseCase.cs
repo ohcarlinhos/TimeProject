9fbf6ad949bdac41008233d261104e2f96a9a411
Carlos Júnior <crpu4a@gmail.com>
2024-10-25 16:24:55 -0300

    feat: create SetIsVerifiedUserUseCase

---
+++ API/Modules/User/UseCases/SetIsVerifiedUserUseCase.cs
@@ -0,0 +1,25 @@
+﻿using API.Core.User;
+using API.Core.User.UseCases;
+using API.Infra.Errors;
+using Shared.General;
+
+namespace API.Modules.User.UseCases;
+
+public class SetIsVerifiedUserUseCase(IUserRepository repo) : ISetIsVerifiedUserUseCase
+{
+    public async Task<Result<bool>> Handle(int id, bool isVerified)
+    {
+        var result = new Result<bool>();
+        var user = await repo.FindById(id);
+
+        if (user == null)
+            return result.SetError(UserMessageErrors.NotFound);
+
+        user.IsVerified = isVerified;
+        await repo.Update(user);
+
+        result.Data = user.IsVerified;
+
+        return result;
+    }
+}
