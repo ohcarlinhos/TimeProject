6b8595cb5e57c70779dedfefb9262d0dd05305a0
Carlos Júnior <crpu4a@gmail.com>
2024-10-25 16:34:42 -0300

    feat: IsVerified to User entity

--- API/Database/Configurations/UserConfiguration.cs
+++ API/Database/Configurations/UserConfiguration.cs
@@ -4,22 +4,23 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace API.Database.Configurations;
 
-public class UserConfiguration: IEntityTypeConfiguration<UserEntity>
+public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
 {
     public void Configure(EntityTypeBuilder<UserEntity> builder)
     {
         builder.ToTable("users");
         builder.HasKey(e => e.Id);
         builder.HasIndex(e => e.Email).IsUnique();
-        
+
         builder.Property(e => e.Id).ValueGeneratedOnAdd().IsRequired();
         builder.Property(e => e.Name).HasMaxLength(120).IsRequired();
         builder.Property(e => e.Email).HasMaxLength(64).IsRequired();
         builder.Property(e => e.Password).HasMaxLength(72).IsRequired();
         builder.Property(e => e.UserRole).IsRequired();
         builder.Property(e => e.IsActive).IsRequired();
-        
+        builder.Property(e => e.IsVerified).IsRequired();
+
         builder.Property(e => e.CreatedAt).IsRequired();
-        builder.Property(e => e.UpdatedAt).IsRequired();    
+        builder.Property(e => e.UpdatedAt).IsRequired();
     }
 }
