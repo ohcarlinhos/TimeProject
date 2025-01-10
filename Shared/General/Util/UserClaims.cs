e06ce59371346144224e6dba81720c1018d88b4c
Carlos Júnior <crpu4a@gmail.com>
2025-01-10 00:46:50 -0300

    feat: adição de novos metodos a UserClaims

--- Shared/General/Util/UserClaims.cs
+++ Shared/General/Util/UserClaims.cs
@@ -11,9 +11,20 @@ public static class UserClaims
             .Claims.FirstOrDefault(claim => claim.Type == "id")?.Value ?? "-1");
     }
 
+    public static string Name(ClaimsPrincipal userClaimsPrincipal)
+    {
+        return userClaimsPrincipal
+            .Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Name)?.Value ?? "";
+    }
+
     public static string Email(ClaimsPrincipal userClaimsPrincipal)
     {
         return userClaimsPrincipal.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email)?.Value ?? "";
+    }
+
+    public static bool IsVerified(ClaimsPrincipal userClaimsPrincipal)
+    {
+        return bool.Parse(userClaimsPrincipal.Claims.FirstOrDefault(claim => claim.Type == "isVerified")?.Value ?? "False");
     }
 
     public static string Role(ClaimsPrincipal userClaimsPrincipal)
