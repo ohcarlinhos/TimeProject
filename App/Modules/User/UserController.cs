3d8d09a877f58707f9a7004cd9d65fe9e8ce39f1
Carlos Júnior <crpu4a@gmail.com>
2025-06-23 11:14:17 -0300

    feat: disable user routes

--- App/Modules/User/UserController.cs
+++ App/Modules/User/UserController.cs
@@ -1,5 +1,4 @@
-﻿using App.Infrastructure.Attributes;
-using Core.User.UseCases;
+﻿using Core.User.UseCases;
 using App.Infrastructure.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,15 +11,12 @@ namespace App.Modules.User;
 [ApiController]
 [Route("api/users")]
 public class UserController(
-    ICreateUserUseCase createUserUseCase,
     IUpdateUserUseCase updateUserUseCase,
     IUpdateUserRoleUseCase updateUserRoleUseCase,
     IDisableUserUseCase disableUserUseCase,
     IGetUserUseCase getUserUseCase,
     IDeleteUserUseCase deleteUserUseCase,
-    IGetPaginatedUserUseCase getPaginatedUserUseCase,
-    ISendRecoveryEmailUseCase sendRecoveryEmailUseCase,
-    ISendRegisterEmailUseCase sendRegisterEmailUseCase
+    IGetPaginatedUserUseCase getPaginatedUserUseCase
 ) : CustomController
 {
     [HttpGet]
@@ -30,13 +26,13 @@ public class UserController(
         return HandleResponse(getPaginatedUserUseCase.Handle(paginationQuery));
     }
 
-    [HttpPost, UserChallenge]
-    public async Task<ActionResult<CreateUserResult>> Create([FromBody] CreateUserDto dto)
-    {
-        var result = await createUserUseCase.Handle(dto);
-        result.ActionName = nameof(Create);
-        return HandleResponse(result);
-    }
+    // [HttpPost, UserChallenge]
+    // public async Task<ActionResult<CreateUserResult>> Create([FromBody] CreateUserDto dto)
+    // {
+    //     var result = await createUserUseCase.Handle(dto);
+    //     result.ActionName = nameof(Create);
+    //     return HandleResponse(result);
+    // }
 
     [HttpPut("{id:int}")]
     [Authorize(Policy = "IsActive")]
@@ -52,8 +48,8 @@ public class UserController(
         return HandleResponse(await updateUserRoleUseCase.Handle(id, dto));
     }
 
-    [Authorize(Policy = "IsActive")]
     [HttpPost("disable/{id:int}")]
+    [Authorize(Policy = "IsActive")]
     public async Task<ActionResult<bool>> Disable([FromRoute] int id, [FromBody] DisableUserDto dto)
     {
         return HasAuthorization(id) ? HandleResponse(await disableUserUseCase.Handle(id, dto)) : Forbid();
@@ -66,31 +62,32 @@ public class UserController(
         return HasAuthorization(id) ? HandleResponse(await deleteUserUseCase.Handle(id)) : Forbid();
     }
 
-    [HttpGet, Route("{id:int}")]
+    [HttpGet("{id:int}")]
     [Authorize(Policy = "IsActive")]
     public async Task<ActionResult<UserMap>> Get(int id)
     {
         return HasAuthorization(id) ? HandleResponse(await getUserUseCase.Handle(id)) : Forbid();
     }
 
-    [HttpGet, Authorize(Policy = "IsActive"), Route("myself")]
+    [HttpGet("myself")]
+    [Authorize(Policy = "IsActive")]
     public async Task<ActionResult<UserMap>> Myself()
     {
         return HandleResponse(await getUserUseCase.Handle(UserClaims.Id(User)));
     }
     
-    [HttpPost, Route("recovery")]
-    public async Task<ActionResult<bool>> Recovery([FromBody] RecoveryDto dto)
-    {
-        return HandleResponse(await sendRecoveryEmailUseCase.Handle(dto.Email));
-    }
+    // [HttpPost, Route("recovery")]
+    // public async Task<ActionResult<bool>> Recovery([FromBody] RecoveryDto dto)
+    // {
+    //     return HandleResponse(await sendRecoveryEmailUseCase.Handle(dto.Email));
+    // }
 
-    [HttpPost, Route("verify")]
-    [Authorize(Policy = "IsActive")]
-    public async Task<ActionResult<bool>> Verify()
-    {
-        return HandleResponse(await sendRegisterEmailUseCase.Handle(UserClaims.Email(User)));
-    }
+    // [HttpPost, Route("verify")]
+    // [Authorize(Policy = "IsActive")]
+    // public async Task<ActionResult<bool>> Verify()
+    // {
+    //     return HandleResponse(await sendRegisterEmailUseCase.Handle(UserClaims.Email(User)));
+    // }
 
     // [HttpPost, Route("verify/{code}")]
     // [Authorize(Policy = "IsActive")]
