f02fb1ec276d38d6e7b8241587ed61c2e8dd58e2
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2026-01-12 23:04:23 -0300

    chore: configuração do scalar

--- TimeProject.APIs/Configurations/SwaggerConfiguration.cs
+++ TimeProject.APIs/Configurations/SwaggerConfiguration.cs
@@ -1,38 +1,38 @@
-﻿using Microsoft.OpenApi.Models;
-
-namespace TimeProject.APIs.Configurations;
-
-public static class SwaggerConfiguration
-{
-    public static void AddSwaggerConfig(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Name = "Authorization",
-                In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer"
-            });
-
-            options.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header
-                    },
-                    new List<string>()
-                }
-            });
-        });
-    }
-}
+﻿// using Microsoft.OpenApi.Models;
+//
+// namespace TimeProject.APIs.Configurations;
+//
+// public static class SwaggerConfiguration
+// {
+//     public static void AddSwaggerConfig(this WebApplicationBuilder builder)
+//     {
+//         builder.Services.AddSwaggerGen(options =>
+//         {
+//             options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//             {
+//                 Name = "Authorization",
+//                 In = ParameterLocation.Header,
+//                 Type = SecuritySchemeType.ApiKey,
+//                 Scheme = "Bearer"
+//             });
+//
+//             options.AddSecurityRequirement(new OpenApiSecurityRequirement
+//             {
+//                 {
+//                     new OpenApiSecurityScheme
+//                     {
+//                         Reference = new OpenApiReference
+//                         {
+//                             Type = ReferenceType.SecurityScheme,
+//                             Id = "Bearer"
+//                         },
+//                         Scheme = "oauth2",
+//                         Name = "Bearer",
+//                         In = ParameterLocation.Header
+//                     },
+//                     new List<string>()
+//                 }
+//             });
+//         });
+//     }
+// }
--- TimeProject.APIs/README.md
+++ TimeProject.APIs/README.md
@@ -3,11 +3,11 @@
 ## Postgres
 
 ```
-docker run --name rmta_db -e POSTGRES_PASSWORD=1234 -p 5432:5432 -d postgres
+docker run --name TimeProject -e POSTGRES_PASSWORD=1234 -p 5432:5432 -d postgres
 ```
 
 ## Migrations
 
 ```
-dotnet ef database update --project App
+dotnet ef database update --project TimeProject.APIs
 ```
